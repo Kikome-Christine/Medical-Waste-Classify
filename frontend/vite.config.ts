8ccdd60844084decc8e4d01171a98c01abe6b721
kikome christine <kikomechristine2022@gmail.com>
2025-06-16 12:18:40 +0300

    changes on the frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://medical-waste-classify-1.onrender.com/',
         changeOrigin: true,
       },
     },
